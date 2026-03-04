f56648dba0bc4e6b26875dea1ea58efb107b45aa
Justin Coffi <21188264+jcoffi@users.noreply.github.com>
2026-03-04 14:58:56 -0800

    Update startup.sh
    Signed-off-by: Justin Coffi <21188264+jcoffi@users.noreply.github.com>

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -72,7 +72,7 @@ export shm_memory="${shm_memory}G"
 export ray_object_store=${ray_object_store}
 
 #Disabled the TLS for ray because it requires the port in the cert name.
-export RAY_USE_TLS=0
+export RAY_USE_TLS=1
 export RAY_TLS_SERVER_CERT=/data/certs/${HOSTNAME,,}.chimp-beta.ts.net.crt
 export RAY_TLS_SERVER_KEY=/data/certs/${HOSTNAME,,}.chimp-beta.ts.net.key
 
